008991110c130fecabb2058b71718d0c02781433
lanangwisana <lanangwisana@gmail.com>
2023-12-02 14:02:48 +0700

    mengisi bagian Model Author

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Author extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $table = 'penulis';
+    protected $primaryKey = 'id';
+    protected $fillable =
+    [
+        'email',
+        'username',
+        'password',
+        'tanggalbergabung'
+    ];
 }
